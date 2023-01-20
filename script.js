6879ba8c5eb2808fb21c5d685984b32289f2e1e1
Nurdoolot <nshanyev@gmail.com>
2023-01-20 21:39:14 +0600

    Added new js file

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const input = document.querySelector('.description');
-const text = document.querySelector('.text');
-const btn = document.querySelector('button');
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+// const classCategory = document.getElementById('added')
 
-function innerText(){
-btn.addEventListener('click', ()  =>{
-    text.innerHTML += input.value;
-})
-}
-innerText();
+// Event listener for add button ------------------------------------------------> 
+addTask.addEventListener('click', function(){
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText= `${inputTask.value}`;
+    task.appendChild(li);
+
+    // let catG = document.createElement('li');
+    // catG.innerText = `${classCategory.value}`;
+    // task.appendChild(li)
+
+    if(inputTask.value === ""){
+        alert("Please enter a task !")
+    } else {
+        taskContainer.appendChild(task);
+    }
+});
