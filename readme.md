01ef1f1d9a9c1cbcf81f42d058c77eb6fcdbb153
Nurdoolot <nshanyev@gmail.com>
2023-01-25 22:37:59 +0600

    Almost all files have been rewritten

--- readme.md
+++
@@ -1,2 +0,0 @@
-I had a task to create the above site based on a small layout (or rather an image). These files are added to improve skills and understanding of HTML and CSS.
-Thank you for your attention 
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// let countdown;
+
+// document.getElementById("start-timer-btn").addEventListener("click", function() {
+//   let seconds = 0;
+//   let minutes = 25;
+
+//   countdown = setInterval(function() {
+//     seconds--;
+//     if (seconds < 0) {
+//       seconds = 59;
+//       minutes--;
+//     }
+//     if(minutes < 0) {
+//         clearInterval(countdown);
+//         alert("Time's up!");
+//     }
+//     document.getElementById("timer").innerHTML = (minutes > 9 ? minutes : "0" + minutes) + ":" + (seconds > 9 ? seconds : "0" + seconds);
+//   }, 1000);
+// });
+
+// document.getElementById("stop-timer-btn").addEventListener("click", function() {
+//   clearInterval(countdown);
+// });
+
 const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
@@ -29,5 +53,4 @@ addTask.addEventListener('click', function(){
         task.remove();
     });
     taskContainer.appendChild(task);
-    
 });
